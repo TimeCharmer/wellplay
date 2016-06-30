240a8d61e9ee9e539080a8d1af95ffb6885783e7
WeiHaoran <2461632167@qq.com>
2016-06-30 20:48:01 +0800

    test the pprint

--- ladies_never.py
+++ ladies_never.py
@@ -4,4 +4,10 @@ Created on 2016年6月28日
 
 @author: Administrator
 '''
+import pprint
 print 'why you are so diao'
+def printmethod():
+    pprint.pprint("why yourda nfdalf")
+
+if __name__=='__main__':
+    printmethod()
